9f8ab550da171d1b1989ba7329633e94fe3a6ac3
mai <uedamai813@gmail.com>
2025-01-13 21:26:54 +0900

    typeof window !== 'undefined'

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -1,20 +1,33 @@
 'use client';
 
 export default function AboutPage() {
-  const utterance = new SpeechSynthesisUtterance();
+  let utterance: SpeechSynthesisUtterance | null = null;
+
+  // ブラウザ環境でのみインスタンスを初期化
+  if (typeof window !== 'undefined') {
+    utterance = new SpeechSynthesisUtterance();
+  }
 
   const speakTest = () => {
-    utterance.text = 'テスト音声を出力します';
-    utterance.pitch = 1;
-    utterance.rate = 1;
-    utterance.volume = 2;
-    speechSynthesis.speak(utterance);
+    if (utterance) {
+      utterance.text = 'テスト音声を出力します';
+      utterance.lang = 'ja-JP';
+      utterance.pitch = 1;
+      utterance.rate = 0.4;
+      utterance.volume = 1;
+      speechSynthesis.speak(utterance);
+    } else {
+      console.error(
+        'SpeechSynthesisUtterance is not supported in this environment.'
+      );
+    }
   };
+
   return (
     <div>
       <h1>About</h1>
       <p>This is the about page</p>
-      <button onClick={() => speakTest()}>test</button>
+      <button onClick={speakTest}>test</button>
     </div>
   );
 }
